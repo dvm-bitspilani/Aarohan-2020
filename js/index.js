e57fdc975f0fc695e52f6af70da3fb51c71cb73b
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2020-08-25 16:05:05 +0530

    mobile site added

--- js/index.js
+++ js/index.js
@@ -1,26 +1,5 @@
-// let scrollPos = 0;
-// let isScrolled = false;
-// let about = document.querySelector('.aboutContainer')
-// // adding scroll event
-// window.addEventListener('wheel', function() {
-//     // detects new state and compares it with the new one
-//     about.style.marginTop = '100vh'
-//     if (((document.body.getBoundingClientRect()).top > scrollPos)){
-//         if(isScrolled || document.body.getBoundingClientRect().top< 10){
-//             about.style.marginTop = '100vh'
-//             setTimeout(() => {
-//                 isScrolled = false
-//             }, 100)
-//             console.log('a')
-//         }
-//     }
-//     else if(document.body.getBoundingClientRect().top> 10 && !isScrolled) {
-//         about.style.marginTop = '0';
-//         console.log('b')
-//         setTimeout(() => {
-//             isScrolled = true
-//         }, 100)
-//     }
-//     // saves the new position for iteration.
-//     scrollPos = (document.body.getBoundingClientRect()).top;
-// })
+const swiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
