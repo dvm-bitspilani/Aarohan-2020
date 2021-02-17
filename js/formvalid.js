05d41425cdd90438ffa07d15f982c2ff1d57798d
Manan Agrawal <44023521+mananag@users.noreply.github.com>
2021-02-17 13:11:57 +0530

    Registrations closed

--- js/formvalid.js
+++ js/formvalid.js
@@ -7,109 +7,112 @@ let obj = {
 
 
 var form = document.forms.namedItem("studentreg");
-function handleForm(event) { 
+function handleForm(event) {
     event.preventDefault();
-    oData = new FormData(form);
-    var object = {};
-    oData.forEach((value, key) => {object[key] = value});
-    var json = JSON.stringify(object);
-    fetch("https://bits-apogee.org/2021/aarohan/studentreg/", {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: json,
-    })
-    .then((res) => {
-      const promise = Promise.resolve(res.json());
-      promise.then((response) => {
-        if(response.message == "Student registered.")
-        {
-          obj.email_id = response.email_id;
-          //obj.paid = response.paid;
-          obj.reg_type = response.reg_type;
-          obj = JSON.stringify(obj);
-          console.log(obj)
-          fetch("https://bits-apogee.org/2021/aarohan/payment/", {
-            method: "POST",
-            headers: { "content-type": "application/json"},
-            body: obj,
-            })
-            .then((res) =>res.text())          
-            
-            .then(text => {
-              console.log(String(text));
-              var opened;
-              
-                
-              window.document.write(String(text))
-            
-              
-              
-              
-              
-            })
-            .catch((err) => {
-            console.log(err)
-            })
-        }
-        else
-          alert(response.message)
-      });
-    })
-    .catch((err) => {
-    console.log(err.message)
-    })
+    // oData = new FormData(form);
+    // var object = {};
+    // oData.forEach((value, key) => {object[key] = value});
+    // var json = JSON.stringify(object);
+    // fetch("https://bits-apogee.org/2021/aarohan/studentreg/", {
+    // method: "POST",
+    // headers: { "content-type": "application/json" },
+    // body: json,
+    // })
+    // .then((res) => {
+    //   const promise = Promise.resolve(res.json());
+    //   promise.then((response) => {
+    //     if(response.message == "Student registered.")
+    //     {
+    //       obj.email_id = response.email_id;
+    //       //obj.paid = response.paid;
+    //       obj.reg_type = response.reg_type;
+    //       obj = JSON.stringify(obj);
+    //       console.log(obj)
+    //       fetch("https://bits-apogee.org/2021/aarohan/payment/", {
+    //         method: "POST",
+    //         headers: { "content-type": "application/json"},
+    //         body: obj,
+    //         })
+    //         .then((res) =>res.text())
+    //
+    //         .then(text => {
+    //           console.log(String(text));
+    //           var opened;
+    //
+    //
+    //           window.document.write(String(text))
+    //
+    //
+    //
+    //
+    //
+    //         })
+    //         .catch((err) => {
+    //         console.log(err)
+    //         })
+    //     }
+    //     else
+    //       alert(response.message)
+    //   });
+    // })
+    // .catch((err) => {
+    // console.log(err.message)
+    // })
+    alert('Registrations are closed')
 }
 form.addEventListener('submit', handleForm);
 
 
 
 var form2 = document.forms.namedItem("schoolreg");
-function handleForm2(event) { 
+function handleForm2(event) {
     event.preventDefault();
-    oData = new FormData(form2);
-    var object = {};
-    oData.forEach((value, key) => {object[key] = value});
-    var json = JSON.stringify(object);
-    //console.log(json)
-    fetch("https://bits-apogee.org/2021/aarohan/schoolreg/", {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: json,
-    })
-    .then((res) => {
-      const promise = Promise.resolve(res.json());
-      promise.then((response) => {
-        if(response.message == "School created.")
-        {
-          obj.email_id = response.email_id;
-          //obj.paid = response.paid;
-          obj.reg_type = response.reg_type;
-          obj = JSON.stringify(obj);
-          console.log(obj)
-          fetch("https://bits-apogee.org/2021/aarohan/payment/", {
-            method: "POST",
-            headers: { "content-type": "application/json"},
-            body: obj,
-            })
-            .then((res) =>res.text())          
-            
-            .then(text => {
-              console.log(String(text))
-              var opened = window.open("",'_blank')
-              opened.document.write(String(text))
-              window.location.reload();
-            })
-            .catch((err) => {
-            console.log(err)
-            })
+    // oData = new FormData(form2);
+    // var object = {};
+    // oData.forEach((value, key) => {object[key] = value});
+    // var json = JSON.stringify(object);
+    // //console.log(json)
+    // fetch("https://bits-apogee.org/2021/aarohan/schoolreg/", {
+    // method: "POST",
+    // headers: { "content-type": "application/json" },
+    // body: json,
+    // })
+    // .then((res) => {
+    //   const promise = Promise.resolve(res.json());
+    //   promise.then((response) => {
+    //     if(response.message == "School created.")
+    //     {
+    //       obj.email_id = response.email_id;
+    //       //obj.paid = response.paid;
+    //       obj.reg_type = response.reg_type;
+    //       obj = JSON.stringify(obj);
+    //       console.log(obj)
+    //       fetch("https://bits-apogee.org/2021/aarohan/payment/", {
+    //         method: "POST",
+    //         headers: { "content-type": "application/json"},
+    //         body: obj,
+    //         })
+    //         .then((res) =>res.text())
+    //
+    //         .then(text => {
+    //           console.log(String(text))
+    //           var opened = window.open("",'_blank')
+    //           opened.document.write(String(text))
+    //           window.location.reload();
+    //         })
+    //         .catch((err) => {
+    //         console.log(err)
+    //         })
+    //
+    //     }
+    //     alert(response.message)
+    //   });
+    // })
+    // .catch((err) => {
+    // console.log(err.message)
+    // })
+    alert('Registrations are closed')
 
-        }
-        alert(response.message)
-      });
-    })
-    .catch((err) => {
-    console.log(err.message)
-    })
 }
 const cancel = () => {
   location.reload(true);
