cf62bfd2416a5c7df3f13fef6ddcfdcc9aa3f114
Saksham Aggarwal <f20201508@pilani.bits-pilani.ac.in>
2022-02-03 13:15:20 +0530

    add: result page functionality

--- js/result.js
+++ js/result.js
@@ -1,40 +1,53 @@
 let form = document.forms.namedItem("result");
 function handleForm(event) {
-    event.preventDefault();
-    oData = new FormData(form);
-    document.querySelector('table').style.display = 'none'
-    document.querySelector('.loader').style.display = 'block'
+  console.log("Event here");
+  event.preventDefault();
 
-    var object = {};
-    oData.forEach((value, key) => {object[key] = value});
-    var json = JSON.stringify(object);
-    fetch("https://bits-apogee.org/2021/aarohan/result/", {
-        method: "POST",
-        headers: { "content-type": "application/json" },
-        body: json,
+  document.querySelector("table").style.display = "none";
+  document.querySelector(".loader").style.display = "block";
+
+  console.log(document.querySelector("#mobile").value);
+
+  const formData = new FormData();
+  formData.append("email", document.querySelector("#email").value);
+  formData.append("contact_no", document.querySelector("#mobile").value);
+
+  const object = {
+    email: document.querySelector("#email").value,
+    contact_no: document.querySelector("#mobile").value
+  };
+  const data = JSON.stringify(object);
+  console.log(data);
+
+  fetch("https://bits-apogee.org/aarohan/result/", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: data,
+  })
+    .then((res) => {
+      const promise = Promise.resolve(res.json());
+      console.log(promise);
+      // console.log(res);
+      promise.then((result) => {
+
+          try {
+              document.getElementById('resultName').innerHTML = result.name;
+              document.getElementById('resultRank').innerHTML = result.all_india_rank;
+              document.querySelector('.loader').style.display = 'none'
+              document.querySelector('table').style.display = 'table'
+          }
+          catch (e){
+              window.alert(response.message)
+              document.querySelector('.loader').style.display = 'none'
+              document.querySelector('table').style.display = 'none'
+          }
+      });
     })
-        .then((res) => {
-            const promise = Promise.resolve(res.json());
-            promise.then((response) => {
-
-                try {
-                    let result = response.data[0]
-                    document.getElementById('resultName').innerHTML = result.name;
-                    document.getElementById('resultRank').innerHTML = result.all_india_rank;
-                    document.querySelector('.loader').style.display = 'none'
-                    document.querySelector('table').style.display = 'table'
-                }
-                catch (e){
-                    window.alert(response.message)
-                    document.querySelector('.loader').style.display = 'none'
-                    document.querySelector('table').style.display = 'none'
-                }
-            });
-        })
-        .catch((err) => {
-            alert(err.message)
-            document.querySelector('.loader').style.display = 'none'
-            document.querySelector('table').style.display = 'none'
-        })
+    .catch((err) => {
+      alert(err.message);
+      console.log("CATCH");
+      document.querySelector(".loader").style.display = "none";
+      document.querySelector("table").style.display = "none";
+    });
 }
-form.addEventListener('submit', handleForm);
+form.addEventListener("submit", handleForm);
