56980df1a3572672a304e99a80a34d1e695bf0ae
Jay Goyal <f20212418@pilani.bits-pilani.ac.in>
2022-10-03 18:38:08 +0530

    Query fix

--- js/contactUs.js
+++ js/contactUs.js
@@ -1,23 +1,27 @@
-var form = document.forms.namedItem("query");
-function handleForm(event) { 
-    event.preventDefault();
-    oData = new FormData(form);
-    var object = {};
-    oData.forEach((value, key) => {object[key] = value});
-    var json = JSON.stringify(object);
-    fetch("https://bits-apogee.org/2021/aarohan/ask_query/", {
-    method: "POST",
-    headers: { "content-type": "application/json" },
-    body: json,
-    })
-    .then((res) => {
-      const promise = Promise.resolve(res.json());
-      promise.then((response) => {
-        alert(response.message)
-      });
-    })
-    .catch((err) => {
-    console.log(err.message)
-    })
+var queryForm = document.forms.namedItem("query");
+
+function handleForm(event) {
+	event.preventDefault();
+	oData = new FormData(queryForm);
+	var object = {};
+	oData.forEach((value, key) => {
+		object[key] = value;
+	});
+	var json = JSON.stringify(object);
+	fetch("https://bits-apogee.org/2021/aarohan/ask_query/", {
+		method: "POST",
+		headers: {"content-type": "application/json"},
+		body: json,
+	})
+		.then((res) => {
+			const promise = Promise.resolve(res.json());
+			promise.then((response) => {
+				alert(response.message);
+			});
+		})
+		.catch((err) => {
+			console.log(err.message);
+		});
 }
-form.addEventListener('submit', handleForm);
+
+queryForm.addEventListener("submit", handleForm);
